59d87dcbe9b15bb9c39c26fbaf5e419d78ff5dae
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-09-16 17:11:29 +0200

    Added App UI component.

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,13 +3,15 @@ import React from "react";
 import {HeroUIProvider} from "@heroui/react";
 
 type AppProps = {
-    children:React.ReactNode
+    children:React.ReactElement
 }
 const BlogForge = ({children}:AppProps) => {
     return (
         <>
             <HeroUIProvider>
-                {children}
+                <div className="flex flex-col flex-wrap space-y-1 pageBody content-start min-h-screen">
+                    {children}
+                </div>
             </HeroUIProvider>
         </>
     )
