27965e360e279f05e3b362875464a2d15d98f067
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-09-16 17:09:12 +0200

    Created SettingsFormType types.

--- src/Types/SettingsFormType.ts
+++ src/Types/SettingsFormType.ts
@@ -1,2 +1,23 @@
+
 export type AllowedSettingFormType = "website_name" | "registration_enabled" | "verify_email_address" | "registration_min_age" | "website_logo";
 export type SettingsFormType = Partial<Record<AllowedSettingFormType, string | number | null | Blob | boolean>>
+export type SettingData = {
+    id:number
+    website_name:string
+    registration_enabled:boolean
+    verify_email_address:boolean
+    registration_min_age:number
+    website_logo?:number
+    tos_text?:string
+    privacy_text?:string
+}
+export type ResponseData = {
+    setting?:SettingData
+    image?:ImageData
+    hasErrors?:boolean
+    message?:string
+}
+export type ImageData = {
+    mime_type:string
+    blob:string
+}
